54478930f60f0af2b86a0ff016dd500cab373bed
WONGI HONG <dt95@naver.com>
2023-05-12 18:42:45 +0900

    무한 스크롤 연습

---
+++ InfiniteScroll/ViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let limit: Int = 149
+    var array = Array<Int>(70...90)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.prefetchDataSource = self
+    }
+    
+    func update() {
+        guard let last = array.last else { return }
+        // 여기에 조건이 있어야함
+        let start = last + 1
+        let end = last + 10
+        let appendedArray = array + Array<Int>(start...end)
+        
+        guard let appendedLast = appendedArray.last else { return }
+        
+        if appendedLast <= limit {
+            array = appendedArray
+            array += Array<Int>(appendedLast + 1...limit)
+            tableView.reloadData()
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = String(array[indexPath.row])
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        update()
+    }
+}
+
+
